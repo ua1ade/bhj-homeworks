50512628f32177b15cb7db1ee2fca85ac95062aa
ua1ade <46313173+ua1ade@users.noreply.github.com>
2019-06-10 17:58:39 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,11 @@
 const stopwatch=function(){
     const timer=document.getElementById("timer");
       timer.textContent--;
-    setInterval(stopwatch, 1000);
-  if (timer < 0) { 
-    clearInterval(stopwatch);
+    setTimeout(stopwatch, 1000);
+  if (timer.textContent==-1) { 
+    clearTimeout(stopwatch);
     alert("«Вы победили в конкурсе!»");
-  }  
+   }  
 }
 stopwatch();
  
