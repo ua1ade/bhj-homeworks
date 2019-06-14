ed8350792a04855c6bd69ddf348f7944e13dded9
ua1ade <46313173+ua1ade@users.noreply.github.com>
2019-06-15 01:19:46 +0300

    Update task.js
    Замена setTimeout на setInterval.

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,10 @@
 const stopwatch = function() {
-  const timer = document.getElementById("timer");
-    timer.textContent--;
-  setTimeout(stopwatch, 500);
-  if (timer.textContent == 0) {
-  	alert("«Вы победили в конкурсе!»");
-    clearTimeout(stopwatch); 
-    timer.onclick = null;
-  }
+	const timer = document.getElementById("timer");
+	timer.textContent--;
+	setInterval(stopwatch, 1000);
+	if (timer.textContent == 0) {
+		alert("«Вы победили в конкурсе!»");
+		clearInterval(stopwatch); 
+	}
 }
-function();
+stopwatch();
