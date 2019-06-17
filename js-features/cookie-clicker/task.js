8d0318143e0e18312eb9ae8257d406544148ee60
ua1ade <46313173+ua1ade@users.noreply.github.com>
2019-06-17 22:14:35 +0300

    Update task.js
    Подключил "onclick" к "cookie" и убрал лишнее.

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,16 +1,9 @@
 const clicker__counter = document.getElementById("clicker__counter");
-const stopwatch = function() {
-	clicker__counter.textContent++;
-	alert();
-};
-setInterval(stopwatch, 1000);
-
-function changeSizes() {
-const cookie = document.getElementById("cookie").onclick;	
-	if (clicker__counter % 2 == 0) {
-		cookie.width = 400;
-		else cookie.width = 250;
+const cookie = document.getElementById("cookie");
+cookie.onclick = function() {	
+	if (clicker__counter.textContent++ % 2 == 0) {
+		cookie.width = 300;
 	}
-};
-stopwatch();
-changeSizes();
+	else cookie.width = 250;	
+}; 
+cookie.onclick();
