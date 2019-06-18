3af3cb7b5f6a0ffde6d3102da71c0ac90789dc87
ua1ade <46313173+ua1ade@users.noreply.github.com>
2019-06-18 13:25:20 +0300

    Update task.js
    Включил 1 ячейку. Цикл пока не работает.

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,3 +1,10 @@
 const hole = document.getElementById("hole1");
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
+hole.onclick = function(){
+	// for (var i = 1; i < hole.length; i++) {
+		if (hole == hole1 || hole == hole2 || hole == hole3) {
+			dead.textContent++;
+		}
+	// }
+}
