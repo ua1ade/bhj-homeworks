9a4473ca3ef941459be248bcc9f86d5f82a05c43
ua1ade <46313173+ua1ade@users.noreply.github.com>
2019-06-18 17:37:37 +0300

    Update task.js
    Добавил функцию "getHole"

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,10 +1,13 @@
 const hole = document.getElementById("hole1");
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
-hole.onclick = function(){
-	// for (var i = 1; i < hole.length; i++) {
-		if (hole == hole1 || hole == hole2 || hole == hole3) {
-			dead.textContent++;
-		}
-	// }
+hole.onclick = function() {
+	if (hole == hole1) {
+		dead.textContent++;
+	}
 }
+getHole(index) = function() {
+	for (let i = 1; i <= 9; i++) {	
+	}
+}
+getHole(i);
