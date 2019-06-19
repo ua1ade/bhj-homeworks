3a94957519b7e9c70386a33e87424e418bb0e66d
ua1ade <46313173+ua1ade@users.noreply.github.com>
2019-06-19 23:34:45 +0300

    Update task.js
    Вывел икремент ++ из оператора if

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,9 @@
 const clicker__counter = document.getElementById("clicker__counter");
 const cookie = document.getElementById("cookie");
-cookie.onclick = function() {	
-	if (clicker__counter.textContent++ % 2 == 0) {
-		cookie.width = 300;
-	}
-	else cookie.width = 250;	
-}; 
+cookie.onclick = function() {
+    let number = clicker__counter.textContent++;
+    if (number % 2 === 0) {
+        cookie.width = 300;
+    } else cookie.width = 250;
+};
 cookie.onclick();
